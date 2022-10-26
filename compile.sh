8b61e0a5957b43fb45ca2c0db8fbf5810f485da0
Thomas Rory Gummerson <thomas@gummerson.no>
2022-10-26 09:58:03 +0200

    GitHub Actions Attempt #1

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@ rm -r bin
 mkdir -p bin
 deno run -A build.ts
 deno upgrade --version 1.9.2
-deno compile --allow-net --unstable --no-check --lite --target x86_64-unknown-linux-gnu --output bin/sirvey-x86_64-linux   sirvey.ts
-deno compile --allow-net --unstable --no-check --lite --target x86_64-pc-windows-msvc   --output bin/sirvey-x86_64-windows sirvey.ts
-deno compile --allow-net --unstable --no-check --lite --target x86_64-apple-darwin      --output bin/sirvey-x86_64-macos   sirvey.ts
-deno compile --allow-net --unstable --no-check --lite --target aarch64-apple-darwin     --output bin/sirvey-aarch64-macos  sirvey.ts
+deno compile --allow-net --unstable --no-check --lite  --output bin/sirvey-aarch64-macos  --target aarch64-apple-darwin     sirvey.ts
+deno compile --allow-net --unstable --no-check --lite  --output bin/sirvey-x86_64-linux   --target x86_64-unknown-linux-gnu sirvey.ts
+deno compile --allow-net --unstable --no-check --lite  --output bin/sirvey-x86_64-macos   --target x86_64-apple-darwin      sirvey.ts
+deno compile --allow-net --unstable --no-check --lite  --output bin/sirvey-x86_64-windows --target x86_64-pc-windows-msvc   sirvey.ts
 deno upgrade
